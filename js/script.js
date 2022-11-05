d0477497f3bf88882b45831e9d16017b284b29b5
FilPolyakov13041995 <Filipp-p@mail.ru>
2022-11-05 03:22:41 +0300

    update

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
     
     const news = document.querySelector('.news');
     const newsItem = Array.from(news.children);
-    const btnNext = document.querySelectorAll('.btnNext');
+    const btnNext = document.querySelectorAll('#btnNext');
+    const btnPrev = document.querySelector('#btnPrev');
 
     newsItem.forEach((item, index) => {
-
         // скрываем все новочти кроме первой.
         if(index !== 0) {
             item.classList.add('hide');
@@ -28,7 +28,8 @@ window.addEventListener('DOMContentLoaded', () => {
             currentNews.classList.add('hide');
             currentNews.removeAttribute('data-active');
 
-            // показываем следующую новость
+
+            // // показываем следующую новость
             const nextNewsIndex = currentNewsIndex + 1 === newsItem.length ? 0 : currentNewsIndex + 1;
             const nextNews = news.querySelector(`[data-index="${nextNewsIndex}"]`);
             nextNews.classList.remove('hide');
@@ -37,13 +38,22 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-
     
-
-
-
-
-   
+    btnPrev.addEventListener('click', () => {
     
+        // скрываем текущую новость
+        const currentNews = news.querySelector('[data-active]');
+        const currentNewsIndex = +currentNews.dataset.index;
+        currentNews.classList.add('hide');
+        currentNews.removeAttribute('data-active');
+    
+        // показывваем следующий слайд
+        const nextNewsIndex = currentNewsIndex === 0 ? newsItem.length - 1 : currentNewsIndex - 1;
+        const nextNews = news.querySelector(`[data-index="${nextNewsIndex}"]`);
+        nextNews.classList.remove('hide');
+        nextNews.setAttribute('data-active', '');
+    
+    });
+
 });
 
--- js/slider.js
+++ js/slider.js
@@ -1,69 +1,125 @@
-function slider() {
+// function slider() {
 
-    const slider = document.querySelector('#slider');
-    const sliderItem = Array.from(slider.children);
-    const bntN = document.querySelector('#btnN');
-    const bntP = document.querySelector('#btnP');
+//     const slider = document.querySelector('#slider');
+//     const sliderItem = Array.from(slider.children);
+//     const btnN = document.querySelector('#btnN');
+//     const btnP = document.querySelector('#btnP');
 
-    sliderItem.forEach((slide, index) => {
-        if(index !== 0) {
-            slide.classList.add('hidden');
-        }
+//     sliderItem.forEach((slide, index) => {
+//         if(index !== 0) {
+//             slide.classList.add('hidden');
+//         }
 
-        slide.dataset.index = index;
-        sliderItem[0].setAttribute('data-active', '');
+//         slide.dataset.index = index;
+//         sliderItem[0].setAttribute('data-active', '');
 
-        slide.addEventListener('click', () => {
-            // скрываем текущий слайд
-            slide.classList.add('hidden');
-            slide.removeAttribute('data-active');
+//         slide.addEventListener('click', () => {
+//             // скрываем текущий слайд
+//             slide.classList.add('hidden');
+//             slide.removeAttribute('data-active');
     
-            // рассчитываем индекс следующего слайда
-            const nextSlideIndex = index + 1 === sliderItem.length ? 0 : index + 1;
+//             // рассчитываем индекс следующего слайда
+//             const nextSlideIndex = index + 1 === sliderItem.length ? 0 : index + 1;
     
-            // находим следующий слайд
-            const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+//             // находим следующий слайд
+//             const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
     
-            // отображаем следующий слайд
-            nextSlide.classList.remove('hidden');
-            nextSlide.setAttribute('data-active', '');
+//             // отображаем следующий слайд
+//             nextSlide.classList.remove('hidden');
+//             nextSlide.setAttribute('data-active', '');
     
             
-        });
+//         });
 
-    });
+//     });
 
-    bntN.addEventListener('click', () => {
+//     btnN.addEventListener('click', () => {
     
-        // скрываем текущий слайд
-        const currentSlide = slider.querySelector('[data-active]');
-        const currentSlideIndex = +currentSlide.dataset.index;
-        currentSlide.classList.add('hidden');
-        currentSlide.removeAttribute('data-active');
+//         // скрываем текущий слайд
+//         const currentSlide = slider.querySelector('[data-active]');
+//         const currentSlideIndex = +currentSlide.dataset.index;
+//         currentSlide.classList.add('hidden');
+//         currentSlide.removeAttribute('data-active');
     
-        // показывваем следующий слайд
-        const nextSlideIndex = currentSlideIndex + 1 === sliderItem.length ? 0 : currentSlideIndex + 1;
-        const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-        nextSlide.classList.remove('hidden');
-        nextSlide.setAttribute('data-active', '');
+//         // показывваем следующий слайд
+//         const nextSlideIndex = currentSlideIndex + 1 === sliderItem.length ? 0 : currentSlideIndex + 1;
+//         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+//         nextSlide.classList.remove('hidden');
+//         nextSlide.setAttribute('data-active', '');
     
-    });
+//     });
     
-    bntP.addEventListener('click', () => {
+//     btnP.addEventListener('click', () => {
         
-        // скрываем текущий слайд
-        const currentSlide = slider.querySelector('[data-active]');
-        const currentSlideIndex = +currentSlide.dataset.index;
-        currentSlide.classList.add('hidden');
-        currentSlide.removeAttribute('data-active');
+//         // скрываем текущий слайд
+//         const currentSlide = slider.querySelector('[data-active]');
+//         const currentSlideIndex = +currentSlide.dataset.index;
+//         currentSlide.classList.add('hidden');
+//         currentSlide.removeAttribute('data-active');
     
-        // показывваем следующий слайд
-        const nextSlideIndex = currentSlideIndex === 0 ? sliderItem.length - 1 : currentSlideIndex - 1;
-        const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-        nextSlide.classList.remove('hidden');
-        nextSlide.setAttribute('data-active', '');
-    });
+//         // показывваем следующий слайд
+//         const nextSlideIndex = currentSlideIndex === 0 ? sliderItem.length - 1 : currentSlideIndex - 1;
+//         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+//         nextSlide.classList.remove('hidden');
+//         nextSlide.setAttribute('data-active', '');
+//     });
 
-}
+// }
 
-slider();
+// slider();
+
+
+
+// Slider 1
+
+const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+  let slideIndex = 1;
+
+
+
+  showSlides(slideIndex);
+
+
+  if(slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+  } else {
+      total.textContent = slides.length;
+  }
+
+
+  function showSlides(n) {
+      if(n > slides.length) {
+          slideIndex = 1;
+      }
+
+      if(n < 1) {
+          slideIndex = slides.length;
+      }
+
+      slides.forEach(item => item.style.display = 'none');
+
+      slides[slideIndex - 1].style.display = 'block';
+
+      if(slides.length < 10) {
+          current.textContent = `0${slideIndex}`;
+      } else {
+          current.textContent = slideIndex;
+      }
+  }
+
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+
+  prev.addEventListener('click', () => {
+      plusSlides(-1);
+  });
+
+  next.addEventListener('click', () => {
+      plusSlides(+1);
+  });
