767df9e0f2d4ebdafda2879ef49173d42838fe82
FilPolyakov13041995 <Filipp-p@mail.ru>
2022-11-11 14:43:19 +0300

    correction menu burger

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,18 @@
 const menuBurger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 
 menuBurger.addEventListener('click', () => {
     menuBurger.classList.toggle('active');
     menu.classList.toggle('active');
+
+    if(menuBurger.classList.contains('active')) {
+        body.style.position = 'fixed';
+    } else {
+        body.style.position = '';
+    }
+
 });
+
+
+
